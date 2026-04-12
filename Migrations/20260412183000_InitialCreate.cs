2c68a98c3681fed05a6519eb18d439558beced15
phnsilva <phnsilva1@gmail.com>
2026-04-12 19:57:20 -0300

    feat: adicionar migracao inicial do banco de dados

---
+++ Migrations/20260412183000_InitialCreate.cs
@@ -0,0 +1,141 @@
+using System;
+using LocadoraVeiculosApi.Data;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LocadoraVeiculosApi.Migrations
+{
+    [DbContext(typeof(ApplicationContext))]
+    [Migration("20260412183000_InitialCreate")]
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CategoriasVeiculo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    ValorDiariaBase = table.Column<decimal>(type: "decimal(10,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CategoriasVeiculo", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(120)", maxLength: 120, nullable: false),
+                    Cpf = table.Column<string>(type: "nvarchar(14)", maxLength: 14, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(120)", maxLength: 120, nullable: false),
+                    Telefone = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Fabricantes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    PaisOrigem = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fabricantes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Veiculos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Modelo = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    AnoFabricacao = table.Column<int>(type: "int", nullable: false),
+                    QuilometragemAtual = table.Column<int>(type: "int", nullable: false),
+                    Placa = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    FabricanteId = table.Column<int>(type: "int", nullable: false),
+                    CategoriaVeiculoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Veiculos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Veiculos_CategoriasVeiculo_CategoriaVeiculoId",
+                        column: x => x.CategoriaVeiculoId,
+                        principalTable: "CategoriasVeiculo",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Veiculos_Fabricantes_FabricanteId",
+                        column: x => x.FabricanteId,
+                        principalTable: "Fabricantes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Alugueis",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClienteId = table.Column<int>(type: "int", nullable: false),
+                    VeiculoId = table.Column<int>(type: "int", nullable: false),
+                    DataInicio = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DataFimPrevista = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DataDevolucao = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    QuilometragemInicial = table.Column<int>(type: "int", nullable: false),
+                    QuilometragemFinal = table.Column<int>(type: "int", nullable: true),
+                    ValorDiaria = table.Column<decimal>(type: "decimal(10,2)", nullable: false),
+                    ValorTotal = table.Column<decimal>(type: "decimal(10,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Alugueis", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Alugueis_Clientes_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "Clientes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Alugueis_Veiculos_VeiculoId",
+                        column: x => x.VeiculoId,
+                        principalTable: "Veiculos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(name: "IX_Alugueis_ClienteId", table: "Alugueis", column: "ClienteId");
+            migrationBuilder.CreateIndex(name: "IX_Alugueis_VeiculoId", table: "Alugueis", column: "VeiculoId");
+            migrationBuilder.CreateIndex(name: "IX_Clientes_Cpf", table: "Clientes", column: "Cpf", unique: true);
+            migrationBuilder.CreateIndex(name: "IX_Clientes_Email", table: "Clientes", column: "Email", unique: true);
+            migrationBuilder.CreateIndex(name: "IX_Veiculos_CategoriaVeiculoId", table: "Veiculos", column: "CategoriaVeiculoId");
+            migrationBuilder.CreateIndex(name: "IX_Veiculos_FabricanteId", table: "Veiculos", column: "FabricanteId");
+            migrationBuilder.CreateIndex(name: "IX_Veiculos_Placa", table: "Veiculos", column: "Placa", unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(name: "Alugueis");
+            migrationBuilder.DropTable(name: "Clientes");
+            migrationBuilder.DropTable(name: "Veiculos");
+            migrationBuilder.DropTable(name: "CategoriasVeiculo");
+            migrationBuilder.DropTable(name: "Fabricantes");
+        }
+    }
+}
---
+++ Migrations/ApplicationContextModelSnapshot.cs
@@ -0,0 +1,131 @@
+using System;
+using LocadoraVeiculosApi.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace LocadoraVeiculosApi.Migrations
+{
+    [DbContext(typeof(ApplicationContext))]
+    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+#pragma warning disable 612, 618
+            modelBuilder
+                .HasAnnotation("ProductVersion", "8.0.0")
+                .HasAnnotation("Relational:MaxIdentifierLength", 128);
+
+            modelBuilder.Entity("LocadoraVeiculosApi.Models.Aluguel", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int");
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
+                    b.Property<int>("ClienteId").HasColumnType("int");
+                    b.Property<DateTime?>("DataDevolucao").HasColumnType("datetime2");
+                    b.Property<DateTime>("DataFimPrevista").HasColumnType("datetime2");
+                    b.Property<DateTime>("DataInicio").HasColumnType("datetime2");
+                    b.Property<int?>("QuilometragemFinal").HasColumnType("int");
+                    b.Property<int>("QuilometragemInicial").HasColumnType("int");
+                    b.Property<decimal>("ValorDiaria").HasColumnType("decimal(10,2)");
+                    b.Property<decimal>("ValorTotal").HasColumnType("decimal(10,2)");
+                    b.Property<int>("VeiculoId").HasColumnType("int");
+                    b.HasKey("Id");
+                    b.HasIndex("ClienteId");
+                    b.HasIndex("VeiculoId");
+                    b.ToTable("Alugueis", (string)null);
+                });
+
+            modelBuilder.Entity("LocadoraVeiculosApi.Models.CategoriaVeiculo", b =>
+                {
+                    b.Property<int>("Id").ValueGeneratedOnAdd().HasColumnType("int");
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
+                    b.Property<string>("Nome").IsRequired().HasMaxLength(60).HasColumnType("nvarchar(60)");
+                    b.Property<decimal>("ValorDiariaBase").HasColumnType("decimal(10,2)");
+                    b.HasKey("Id");
+                    b.ToTable("CategoriasVeiculo", (string)null);
+                });
+
+            modelBuilder.Entity("LocadoraVeiculosApi.Models.Cliente", b =>
+                {
+                    b.Property<int>("Id").ValueGeneratedOnAdd().HasColumnType("int");
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
+                    b.Property<string>("Cpf").IsRequired().HasMaxLength(14).HasColumnType("nvarchar(14)");
+                    b.Property<string>("Email").IsRequired().HasMaxLength(120).HasColumnType("nvarchar(120)");
+                    b.Property<string>("Nome").IsRequired().HasMaxLength(120).HasColumnType("nvarchar(120)");
+                    b.Property<string>("Telefone").HasMaxLength(20).HasColumnType("nvarchar(20)");
+                    b.HasKey("Id");
+                    b.HasIndex("Cpf").IsUnique();
+                    b.HasIndex("Email").IsUnique();
+                    b.ToTable("Clientes", (string)null);
+                });
+
+            modelBuilder.Entity("LocadoraVeiculosApi.Models.Fabricante", b =>
+                {
+                    b.Property<int>("Id").ValueGeneratedOnAdd().HasColumnType("int");
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
+                    b.Property<string>("Nome").IsRequired().HasMaxLength(100).HasColumnType("nvarchar(100)");
+                    b.Property<string>("PaisOrigem").HasMaxLength(60).HasColumnType("nvarchar(60)");
+                    b.HasKey("Id");
+                    b.ToTable("Fabricantes", (string)null);
+                });
+
+            modelBuilder.Entity("LocadoraVeiculosApi.Models.Veiculo", b =>
+                {
+                    b.Property<int>("Id").ValueGeneratedOnAdd().HasColumnType("int");
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
+                    b.Property<int>("AnoFabricacao").HasColumnType("int");
+                    b.Property<int>("CategoriaVeiculoId").HasColumnType("int");
+                    b.Property<int>("FabricanteId").HasColumnType("int");
+                    b.Property<string>("Modelo").IsRequired().HasMaxLength(100).HasColumnType("nvarchar(100)");
+                    b.Property<string>("Placa").IsRequired().HasMaxLength(10).HasColumnType("nvarchar(10)");
+                    b.Property<int>("QuilometragemAtual").HasColumnType("int");
+                    b.HasKey("Id");
+                    b.HasIndex("CategoriaVeiculoId");
+                    b.HasIndex("FabricanteId");
+                    b.HasIndex("Placa").IsUnique();
+                    b.ToTable("Veiculos", (string)null);
+                });
+
+            modelBuilder.Entity("LocadoraVeiculosApi.Models.Aluguel", b =>
+                {
+                    b.HasOne("LocadoraVeiculosApi.Models.Cliente", "Cliente")
+                        .WithMany("Alugueis")
+                        .HasForeignKey("ClienteId")
+                        .OnDelete(DeleteBehavior.Restrict)
+                        .IsRequired();
+                    b.HasOne("LocadoraVeiculosApi.Models.Veiculo", "Veiculo")
+                        .WithMany("Alugueis")
+                        .HasForeignKey("VeiculoId")
+                        .OnDelete(DeleteBehavior.Restrict)
+                        .IsRequired();
+                    b.Navigation("Cliente");
+                    b.Navigation("Veiculo");
+                });
+
+            modelBuilder.Entity("LocadoraVeiculosApi.Models.Veiculo", b =>
+                {
+                    b.HasOne("LocadoraVeiculosApi.Models.CategoriaVeiculo", "CategoriaVeiculo")
+                        .WithMany("Veiculos")
+                        .HasForeignKey("CategoriaVeiculoId")
+                        .OnDelete(DeleteBehavior.Restrict)
+                        .IsRequired();
+                    b.HasOne("LocadoraVeiculosApi.Models.Fabricante", "Fabricante")
+                        .WithMany("Veiculos")
+                        .HasForeignKey("FabricanteId")
+                        .OnDelete(DeleteBehavior.Restrict)
+                        .IsRequired();
+                    b.Navigation("CategoriaVeiculo");
+                    b.Navigation("Fabricante");
+                });
+
+            modelBuilder.Entity("LocadoraVeiculosApi.Models.CategoriaVeiculo", b => { b.Navigation("Veiculos"); });
+            modelBuilder.Entity("LocadoraVeiculosApi.Models.Cliente", b => { b.Navigation("Alugueis"); });
+            modelBuilder.Entity("LocadoraVeiculosApi.Models.Fabricante", b => { b.Navigation("Veiculos"); });
+#pragma warning restore 612, 618
+        }
+    }
+}
